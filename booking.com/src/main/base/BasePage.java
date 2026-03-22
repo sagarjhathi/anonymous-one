20752161e134a416912f3357bf60ed38783d8758
sagarjhathi <sagarjhathii@gmail.com>
2026-03-22 17:56:13 +0530

    Adding Base test , base pages

--- booking.com/src/main/base/BasePage.java
+++ booking.com/src/main/base/BasePage.java
@@ -1,6 +1,20 @@
 package main.base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import main.driver.DManager;
+
 public class BasePage {
 
+	public WebDriver driver;
+	
+    public BasePage(){
+    	
+    
+    	driver=DManager.getDrivers();
+    	
+    }
+    
     
 }
---
+++ booking.com/src/main/base/BaseTest.java
@@ -0,0 +1,30 @@
+package main.base;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import main.driver.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+
+	public WebDriver driver;
+	
+	@BeforeMethod(alwaysRun=true)
+	public void setup() {
+		
+		DManager.setDriver();
+		driver= main.driver.DManager.getDrivers();
+	
+		
+	}
+	
+	@AfterMethod(alwaysRun=true)
+	public void tearDown() {
+		
+		DManager.removeDriver();
+	}
+}
