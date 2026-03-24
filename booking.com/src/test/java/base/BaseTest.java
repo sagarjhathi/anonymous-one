26f89ecb0af4b65a36a5c2536653a207364b775d
sagarjhathi <sagarjhathii@gmail.com>
2026-03-24 22:33:54 +0530

    Removing all the commented code from the base test for better structuring of code base

--- booking.com/src/test/java/base/BaseTest.java
+++ booking.com/src/test/java/base/BaseTest.java
@@ -19,61 +19,8 @@ import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
 
-	//private static ThreadLocal<String> testFolderPath = new ThreadLocal<>();
-
 	public WebDriver driver;
-	
-//	@BeforeMethod(alwaysRun=true)
-//	public void setup() {
-//		
-//		
-//		DManager.setDriver();
-//		driver= DManager.getDrivers();
-//	
-//		
-//	}
-//	
-//	  @BeforeMethod(alwaysRun = true)
-//	    public void setTestName(ITestResult result) {
-//	        String testName = result.getMethod().getMethodName();
-//	        ThreadContext.put("testName", testName);
-//	        
-//	    }
-	  
-	  
-	  
-
-	  
-	//    private static String runFolderPath;
-
-//	    @BeforeSuite(alwaysRun = true)
-//	    public void createRunFolder() {
-//
-//	    	 
-//
-//	        // Step 1: Create timestamp
-//	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
-//	        String timestamp = LocalDateTime.now().format(formatter);
-//
-//	        // Step 2: Build folder path
-//	        runFolderPath = System.getProperty("user.dir") 
-//	                        + File.separator + "logs" 
-//	                        + File.separator + "run_" + timestamp;
-//
-//	        // Step 3: Create folder
-//	        File folder = new File(runFolderPath);
-//
-//	        if (!folder.exists()) {
-//	            boolean created = folder.mkdirs();
-//
-//	            if (created) {
-//	                System.out.println("Run folder created: " + runFolderPath);
-//	            } else {
-//	                System.out.println("Failed to create run folder!");
-//	            }
-//	        }
-//	    }
-	    
+		    
 	  
 	  @BeforeSuite(alwaysRun = true)
 	  public void createRunFolder() {
@@ -91,69 +38,7 @@ public class BaseTest {
 	      PathManager.setRunFolderPath(path);  // ✅ CRITICAL
 	  }
 
-	    
-	    
-//	    @BeforeMethod(alwaysRun = true)
-//	    public void createTestFolder(ITestResult result) {
-//
-//	        // Step 1: get test name
-//	        String testName = result.getMethod().getMethodName();
-//
-//	        // Step 2: build path
-//	        String path = getRunFolderPath() 
-//	                      + File.separator + testName;
-//
-//	        // Step 3: create folder
-//	        File folder = new File(path);
-//
-//	        if (!folder.exists()) {
-//	            folder.mkdirs();
-//	        }
-//
-//	        // Step 4: store in ThreadLocal
-//	        
-//	        testFolderPath.set(path);
-//	        // 🔥 Generate timestamp
-//	        String timestamp = LocalDateTime.now()
-//	                .format(DateTimeFormatter.ofPattern("HH-mm-ss"));
-//
-//	        // 🔥 Build unique log file name
-//	        String logFileName = testName + "_" + timestamp;
-//	       
-//
-//	        // 🔥 Push into ThreadContext
-//	        ThreadContext.put("logFileName", logFileName);
-//	        ThreadContext.put("logPath", path);
-//	        ThreadContext.put("testName", testName);
-//	        
-//
-//	    }
-	    
-	    
-//	    public static String getScreenshotRunPath() {
-//
-//	        String runName = new File(runFolderPath).getName();
-//
-//	        return System.getProperty("user.dir")
-//	                + File.separator + "screenshots"
-//	                + File.separator + runName;
-//	    }
-//	    
-//	    
-//	    public static String getScreenshotTestPath(String testName) {
-//	        return getScreenshotRunPath() + File.separator + testName;
-//	    }
-//	    
-//	    // Getter (we will use this later)
-//	    public static String getRunFolderPath() {
-//	        return runFolderPath;
-//	    }
-//	
-//	    public static String getTestFolderPath() {
-//	        return testFolderPath.get();
-//	    }
-//	    
-	    
+	    	    
 	    
 	  @BeforeMethod(alwaysRun = true)
 	  public void beforeTest(ITestResult result) {
@@ -182,24 +67,7 @@ public class BaseTest {
 	      ThreadContext.put("testName", testName);
 	  }
 	    
-//	    @AfterMethod(alwaysRun = true)
-//	    public void clearTestFolder() {
-//	        testFolderPath.remove();
-//	    }
-//	    
-//	    
-//	    
-//	    @AfterMethod(alwaysRun = true)
-//	    public void clearContext() {
-//	        ThreadContext.clearAll();
-//	        testFolderPath.remove();
-//	    }
-//	    
-//	@AfterMethod(alwaysRun=true)
-//	public void tearDown() {
-//		
-//		DManager.removeDriver();
-//	}
+
 	  
 	  @AfterMethod(alwaysRun = true)
 	  public void afterTest() {
