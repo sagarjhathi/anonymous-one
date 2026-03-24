875e36db1c8ed128f19f9029e971df83139dec95
sagarjhathi <sagarjhathii@gmail.com>
2026-03-25 00:01:45 +0530

    Checking out the reporting with one test

--- booking.com/src/test/java/base/BaseTest.java
+++ booking.com/src/test/java/base/BaseTest.java
@@ -14,23 +14,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseTest {
 
 	public WebDriver driver;
-		    
+	ExtentReports extent;
 	  
 	  @BeforeSuite(alwaysRun = true)
 	  public void createRunFolder() {
-
+		  extent = new ExtentReports();
 	      String timestamp = LocalDateTime.now()
 	          .format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
 
 	      String path = System.getProperty("user.dir")
 	              + File.separator + "logs"
 	              + File.separator + "run_" + timestamp;
+	      
+	      String extentPath = System.getProperty("user.dir")
+	              + File.separator + "reports"
+	              + File.separator + "run_" + timestamp+File.separator+"ExtentReport.html";
+	      
+	      ExtentSparkReporter reporter =
+	    		    new ExtentSparkReporter(extentPath + File.separator + "report.html");
+
+	    		extent.attachReporter(reporter);
 
 	      File folder = new File(path);
 	      folder.mkdirs();
@@ -61,18 +75,28 @@ public class BaseTest {
 	      // 4. Logging context
 	      String timestamp = LocalDateTime.now()
 	              .format(DateTimeFormatter.ofPattern("HH-mm-ss"));
-
+	      
 	      ThreadContext.put("logFileName", testName + "_" + timestamp);
 	      ThreadContext.put("logPath", path);
 	      ThreadContext.put("testName", testName);
+	      ExtentTest test = extent.createTest(testName);
+	      
 	  }
 	    
 
 	  
 	  @AfterMethod(alwaysRun = true)
 	  public void afterTest() {
+		  
+		  
 	      ThreadContext.clearAll();
 	      PathManager.clearTestFolder();
 	      DManager.removeDriver();
+	  }
+	  
+	  
+	  @AfterSuite
+	  public void flushReport() {
+		  extent.flush();
 	  }
 }
